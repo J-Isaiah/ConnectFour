559a903f44d04ca41f0de891320a178a7431ac3e
Isaiah Jacobsen <isaiah.jacobsen1@gmail.com>
2025-11-24 18:52:58 -0600

    Defined the huristic and evaluation of states

--- src/game_engine/evaluation.py
+++ src/game_engine/evaluation.py
@@ -4,7 +4,40 @@ from board import Board, Cell
 def evaluate(board: Board, player: Cell) -> int:
     opp = Cell.RED if player == Cell.YELLOW else Cell.YELLOW
 
-    rows_score = eval_rows(board=board, player=player, opp=opp)
+    total_score = 0
+    total_score += eval_rows(board=board, player=player, opp=opp)
+    total_score += eval_cols(board=board, player=player, opp=opp)
+    total_score += eval_pos_diagonals(board=board, player=player, opp=opp)
+    total_score += eval_neg_diagonals(board=board, player=player, opp=opp)
+
+    center_col = Board.COLUMNS // 2
+    center_array = [board.grid[r][center_col] for r in range(Board.ROWS)]
+    center_count = center_array.count(player)
+    total_score += center_count * 3
+
+    return total_score
+
+
+def score_window(window, player, opp) -> int:
+    score = 0
+    own = window.count(player)
+    opp_spaces = window.count(opp)
+    empty = window.count(Cell.EMPTY)
+
+    # scoring (same as rows)
+    if own == 4:
+        score += 1000
+    elif own == 3 and empty == 1:
+        score += 50
+    elif own == 2 and empty == 2:
+        score += 25
+
+    if opp_spaces == 3 and empty == 1:
+        score -= 90
+    elif opp_spaces == 2 and empty == 2:
+        score -= 50
+
+    return score
 
 
 def eval_rows(board: Board, player: Cell, opp: Cell):
@@ -14,26 +47,8 @@ def eval_rows(board: Board, player: Cell, opp: Cell):
     for row in range(Board.ROWS):
         for col in range(Board.COLUMNS - 3):
             window = [b[row][col], b[row][col + 1], b[row][col + 2], b[row][col + 3]]
-            own_spaces = window.count(player)
-            empty = window.count(Cell.EMPTY)
-            opp_spaces = window.count(opp)
 
-            if own_spaces == 4:
-                score += 1000
-            elif own_spaces == 3 and empty == 1:
-                score += 50
-
-            elif own_spaces == 2 and empty == 2:
-                score += 25
-
-            elif own_spaces == 1 and empty == 1:
-                score += 10
-
-            if opp_spaces == 3 and empty == 1:
-                score -= 80
-            elif opp_spaces == 2 and empty == 2:
-                score -= 50
-
+            score += score_window(window=window, player=player, opp=opp)
     return score
 
 
@@ -50,28 +65,35 @@ def eval_cols(board: Board, player: Cell, opp: Cell):
                 b[row + 3][col],
             ]
 
-            own = window.count(player)
-            opp_spaces = window.count(opp)
-            empty = window.count(Cell.EMPTY)
-
-            # scoring (same as rows)
-            if own == 4:
-                score += 1000
-            elif own == 3 and empty == 1:
-                score += 50
-            elif own == 2 and empty == 2:
-                score += 25
-
-            if opp_spaces == 3 and empty == 1:
-                score -= 80
-            elif opp_spaces == 2 and empty == 2:
-                score -= 50
-
+            score += score_window(window=window, player=player, opp=opp)
     return score
 
 
-# def eval_pos_diagnals(board:Board, player:Cell, opp:Cell):
-#     b = board.grid
-#     score = 0
-#     for col in range(Board.ROWS):
-#         for row in range(Board.ROWS):
+def eval_pos_diagonals(board: Board, player: Cell, opp: Cell):
+    score = 0
+    for row in range(Board.ROWS - 3):
+        for col in range(Board.COLUMNS - 3):
+            window = [
+                board.grid[row][col],
+                board.grid[row + 1][col + 1],
+                board.grid[row + 2][col + 2],
+                board.grid[row + 3][col + 3],
+            ]
+
+            score += score_window(window, player=player, opp=opp)
+    return score
+
+
+def eval_neg_diagonals(board: Board, player: Cell, opp: Cell):
+    score = 0
+    for row in range(Board.ROWS - 3):
+        for col in range(3, Board.COLUMNS):
+            window = [
+                board.grid[row][col],
+                board.grid[row + 1][col - 1],
+                board.grid[row + 2][col - 2],
+                board.grid[row + 3][col - 3],
+            ]
+
+            score += score_window(window=window, player=player, opp=opp)
+    return score
